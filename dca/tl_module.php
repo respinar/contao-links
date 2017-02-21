9e956f2f26e58d08c058517094c842103f1d4a60
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2017-02-21 19:03:27 +0330

    add sorting

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -3,7 +3,7 @@
 /**
  * Add palettes to tl_module
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['links']   = '{title_legend},name,headline,type;{category_legend},links_categories;{template_legend:hide},links_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['links']   = '{title_legend},name,headline,type;{category_legend},links_categories;{template_legend},links_sortBy,links_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 
 /**
@@ -14,9 +14,21 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['links_categories'] = array
 	'label'                => &$GLOBALS['TL_LANG']['tl_module']['links_categories'],
 	'exclude'              => true,
 	'inputType'            => 'checkbox',
-	'options_callback'     => array('tl_module_links', 'getCategories'),
+	'foreignKey'           => 'tl_links_category.title',
 	'eval'                 => array('multiple'=>true, 'mandatory'=>true),
 	'sql'				   => "blob NULL",
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['links_sortBy'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['links_sortBy'],
+	'default'                 => 'custom',
+	'exclude'                 => true,
+	'inputType'               => 'select',
+	'options'                 => array('custom','date_desc', 'date_asc','title_asc', 'title_desc'),
+	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval'                    => array('tl_class'=>'w50'),
+	'sql'                     => "varchar(16) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['links_template'] = array
@@ -25,7 +37,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['links_template'] = array
 	'exclude'              => true,
 	'inputType'            => 'select',
 	'options_callback'     => array('tl_module_links', 'getLinksTemplates'),
-	'eval'				   => array('tl_class'=>'w50'),
+	'eval'				   => array('tl_class'=>'w50 clr'),
 	'sql'				   => "varchar(64) NOT NULL default ''",
 );
 
@@ -40,33 +52,6 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['links_template'] = array
  */
 class tl_module_links extends Backend
 {
-
-	/**
-	 * Get all links archives and return them as array
-	 * @return array
-	 */
-	public function getCategories()
-	{
-		//if (!$this->User->isAdmin && !is_array($this->User->news))
-		//{
-		//	return array();
-		//}
-
-		$arrCategories = array();
-		$objCategories = $this->Database->execute("SELECT id, title FROM tl_links_category ORDER BY title");
-
-		while ($objCategories->next())
-		{
-			//if ($this->User->hasAccess($objArchives->id, 'news'))
-			//{
-				$arrCategories[$objCategories->id] = $objCategories->title;
-			//}
-		}
-
-		return $arrCategories;
-	}
-
-
 	/**
 	 * Return all links templates as array
 	 * @param object
