b96e799f11a77a65a6fc27302fbf487265a3624d
Hamid Peywasti <hamid@respinar.com>
2023-01-21 11:41:34 +0330

    Create Contao bundle

---
+++ src/RespinarContaoLinksBundle.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Products Bundle.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\ContaoLinks;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RespinarContaoLinksBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
+}
