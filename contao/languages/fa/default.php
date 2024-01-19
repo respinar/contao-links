83e50b579fa0b0d7eb04bcdf0a3090de90425ced
Hamid Peywasti <hamid@respinar.com>
2024-01-19 16:37:11 +0330

    Update License

--- contao/languages/fa/default.php
+++ contao/languages/fa/default.php
@@ -1,16 +1,12 @@
 <?php
-/**
- * Translations are managed using Transifex. To create a new translation
- * or to help to maintain an existing one, please register at transifex.com.
- *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/links/language/fa/
- *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2015-05-29T12:25:35+04:30
- */
 
+/*
+ * This file is part of Contao Links Bundle.
+ *
+ * (c) Hamid Peywasti 2014-2024 <hamid@respinar.com>
+ *
+ * @license LGPL-3.0-or-later
+ */
 
 $GLOBALS['TL_LANG']['MSC']['emptyLinks'] = 'پیوندی وجود ندارد';
 
