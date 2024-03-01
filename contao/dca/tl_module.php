6258300a19b6f6f836129ca201287de8802d2037
Hamid Peywasti <hamid@respinar.com>
2024-03-01 11:07:17 +0330

    remove extra spaces and Prettify

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -11,39 +11,39 @@
 /**
  * Add palettes to tl_module
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['links']   = '{title_legend},name,headline,type;{category_legend},links_categories;{template_legend},links_sortBy,links_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['links'] = '{title_legend},name,headline,type;{category_legend},links_categories;{template_legend},links_sortBy,links_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 
 /**
  * Add fields to tl_module
  */
 $GLOBALS['TL_DCA']['tl_module']['fields']['links_categories'] = [
-	'label'                => &$GLOBALS['TL_LANG']['tl_module']['links_categories'],
-	'exclude'              => true,
-	'inputType'            => 'checkbox',
-	'foreignKey'           => 'tl_links_category.title',
-	'eval'                 => ['multiple'=>true, 'mandatory'=>true],
-	'sql'				   => "blob NULL",
+	'label' => &$GLOBALS['TL_LANG']['tl_module']['links_categories'],
+	'exclude' => true,
+	'inputType' => 'checkbox',
+	'foreignKey' => 'tl_links_category.title',
+	'eval' => ['multiple' => true, 'mandatory' => true],
+	'sql' => "blob NULL",
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['links_sortBy'] = [
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['links_sortBy'],
-	'default'                 => 'custom',
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options'                 => ['custom','date_desc', 'date_asc','title_asc', 'title_desc'],
-	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
-	'eval'                    => ['tl_class'=>'w50'],
-	'sql'                     => "varchar(16) NOT NULL default ''"
+	'label' => &$GLOBALS['TL_LANG']['tl_module']['links_sortBy'],
+	'default' => 'custom',
+	'exclude' => true,
+	'inputType' => 'select',
+	'options' => ['custom','date_desc', 'date_asc','title_asc', 'title_desc'],
+	'reference' => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval' => ['tl_class' => 'w50'],
+	'sql' => "varchar(16) NOT NULL default ''"
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['links_template'] = [
-	'label'                => &$GLOBALS['TL_LANG']['tl_module']['links_template'],
-	'exclude'              => true,
-	'inputType'            => 'select',
-	'options_callback'     => ['tl_module_links', 'getLinksTemplates'],
-	'eval'				   => ['tl_class'=>'w50 clr'],
-	'sql'				   => "varchar(64) NOT NULL default ''",
+	'label' => &$GLOBALS['TL_LANG']['tl_module']['links_template'],
+	'exclude' => true,
+	'inputType' => 'select',
+	'options_callback' => ['tl_module_links', 'getLinksTemplates'],
+	'eval' => ['tl_class' => 'w50 clr'],
+	'sql' => "varchar(64) NOT NULL default ''",
 ];
 
 
