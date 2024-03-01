aa9e013f91abac85a774d2521ce562953705ef39
Hamid Peywasti <hamid@respinar.com>
2024-03-01 11:08:14 +0330

    change .gif to .svg

--- contao/dca/tl_links_category.php
+++ contao/dca/tl_links_category.php
@@ -51,28 +51,28 @@ $GLOBALS['TL_DCA']['tl_links_category'] = [
 			'edit' => [
 				'label' => &$GLOBALS['TL_LANG']['tl_links_category']['edit'],
 				'href' => 'table=tl_links',
-				'icon' => 'edit.gif'
+				'icon' => 'edit.svg'
 			],
 			'editheader' => [
 				'label' => &$GLOBALS['TL_LANG']['tl_links_category']['editheader'],
 				'href' => 'act=edit',
-				'icon' => 'header.gif'
+				'icon' => 'header.svg'
 			],
 			'copy' => [
 				'label' => &$GLOBALS['TL_LANG']['tl_links_category']['copy'],
 				'href' => 'act=copy',
-				'icon' => 'copy.gif'
+				'icon' => 'copy.svg'
 			],
 			'delete' => [
 				'label' => &$GLOBALS['TL_LANG']['tl_links_category']['delete'],
 				'href' => 'act=delete',
-				'icon' => 'delete.gif',
+				'icon' => 'delete.svg',
 				'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
 			],
 			'show' => [
 				'label' => &$GLOBALS['TL_LANG']['tl_links_category']['show'],
 				'href' => 'act=show',
-				'icon' => 'show.gif'
+				'icon' => 'show.svg'
 			]
 		]
 	],
