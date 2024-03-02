c6c8e2dbd3e9298db59df941ccf2346d2719332b
Hamid Peywasti <hamid@respinar.com>
2024-03-02 11:38:36 +0330

    add use and ?? null

--- contao/dca/tl_links.php
+++ contao/dca/tl_links.php
@@ -9,6 +9,8 @@
  */
 
 use Contao\DC_Table;
+use Contao\Backend;
+use Contao\System;
 
  /**
  * Load tl_content language file
@@ -65,7 +67,7 @@ $GLOBALS['TL_DCA']['tl_links'] = [
 			'delete' => [
 				'href' => 'act=delete',
 				'icon' => 'delete.svg',
-				'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+				'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
 			],
 			'toggle' => [
 				'icon' => 'visible.svg',
