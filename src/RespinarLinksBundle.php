427026530141075f30b5cb66e35eac92abe2cc9d
Hamid Peywasti <hamid@respinar.com>
2024-03-03 15:27:28 +0330

    rename file to RespinarLinksBindle

---
+++ src/RespinarLinksBundle.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Links Bundle.
+ *
+ * (c) Hamid Peywasti 2014-2024 <hamid@respinar.com>
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Respinar\LinksBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RespinarContaoLinksBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
+}
