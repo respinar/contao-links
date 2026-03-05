3ad9642cd31f9285e3f792e1ab2b463b3986577c
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-05 23:27:53 +0330

    format by ecs

--- contao/languages/en/tl_links.php
+++ contao/languages/en/tl_links.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -8,41 +10,38 @@
  * @license LGPL-3.0-or-later
  */
 
-
-/**
+/*
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_links']['title']          = array('Title','Please enter the link title.');
-$GLOBALS['TL_LANG']['tl_links']['linkTitle']      = array('Link title','The link title is added as title attribute in the HTML markup.');
-$GLOBALS['TL_LANG']['tl_links']['published']      = array('Publish link','Make the link publicly visible on the website.');
-$GLOBALS['TL_LANG']['tl_links']['rel']            = array('rel','The rel Attribute is added as rel attribute in the HTML markup (e.g. <em>nofollow</em>).');
-$GLOBALS['TL_LANG']['tl_links']['class']          = array('CSS class','The class Attribute is added as class attribute in the HTML markup.');
-$GLOBALS['TL_LANG']['tl_links']['show']           = array('Link details','Show the details of link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['singleSRC']      = array('Source file','Please select a file or folder from the files directory.');
-$GLOBALS['TL_LANG']['tl_links']['target']         = array('Target','Open the link in a new browser window.');
-$GLOBALS['TL_LANG']['tl_links']['url']            = array('URL','Please enter the link url.');
-$GLOBALS['TL_LANG']['tl_links']['addImage']       = array('Add an image','Add an image to the link item.');
-$GLOBALS['TL_LANG']['tl_links']['start']          = array('Show from','Do not show the link item on the website before this day.');
-$GLOBALS['TL_LANG']['tl_links']['stop']           = array('Show until','Do not show the link item on the website on and after this day.');
+$GLOBALS['TL_LANG']['tl_links']['title'] = ['Title', 'Please enter the link title.'];
+$GLOBALS['TL_LANG']['tl_links']['linkTitle'] = ['Link title', 'The link title is added as title attribute in the HTML markup.'];
+$GLOBALS['TL_LANG']['tl_links']['published'] = ['Publish link', 'Make the link publicly visible on the website.'];
+$GLOBALS['TL_LANG']['tl_links']['rel'] = ['rel', 'The rel Attribute is added as rel attribute in the HTML markup (e.g. <em>nofollow</em>).'];
+$GLOBALS['TL_LANG']['tl_links']['class'] = ['CSS class', 'The class Attribute is added as class attribute in the HTML markup.'];
+$GLOBALS['TL_LANG']['tl_links']['show'] = ['Link details', 'Show the details of link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['singleSRC'] = ['Source file', 'Please select a file or folder from the files directory.'];
+$GLOBALS['TL_LANG']['tl_links']['target'] = ['Target', 'Open the link in a new browser window.'];
+$GLOBALS['TL_LANG']['tl_links']['url'] = ['URL', 'Please enter the link url.'];
+$GLOBALS['TL_LANG']['tl_links']['addImage'] = ['Add an image', 'Add an image to the link item.'];
+$GLOBALS['TL_LANG']['tl_links']['start'] = ['Show from', 'Do not show the link item on the website before this day.'];
+$GLOBALS['TL_LANG']['tl_links']['stop'] = ['Show until', 'Do not show the link item on the website on and after this day.'];
 
-
-/**
+/*
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_links']['href_legend']    = 'Hyperlink attributes';
-$GLOBALS['TL_LANG']['tl_links']['image_legend']   = 'Image settings';
-$GLOBALS['TL_LANG']['tl_links']['title_legend']   = 'Title';
+$GLOBALS['TL_LANG']['tl_links']['href_legend'] = 'Hyperlink attributes';
+$GLOBALS['TL_LANG']['tl_links']['image_legend'] = 'Image settings';
+$GLOBALS['TL_LANG']['tl_links']['title_legend'] = 'Title';
 $GLOBALS['TL_LANG']['tl_links']['publish_legend'] = 'Publish Settings';
 
-
-/**
+/*
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_links']['new']            = array('New link','Add a new link');
-$GLOBALS['TL_LANG']['tl_links']['copy']           = array('Copy link','Copy link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['cut']            = array('Move link','Move link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['delete']         = array('Delete link','Delete link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['edit']           = array('Edit link','Edit link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['pasteafter']     = array('Paste into this category','Paste after link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['pastenew']       = array('Add new at the top','Add new after link ID %s');
-$GLOBALS['TL_LANG']['tl_links']['toggle']         = array('Publish/unpublish link','Publish/unpublish link ID %s');
+$GLOBALS['TL_LANG']['tl_links']['new'] = ['New link', 'Add a new link'];
+$GLOBALS['TL_LANG']['tl_links']['copy'] = ['Copy link', 'Copy link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['cut'] = ['Move link', 'Move link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['delete'] = ['Delete link', 'Delete link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['edit'] = ['Edit link', 'Edit link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['pasteafter'] = ['Paste into this category', 'Paste after link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['pastenew'] = ['Add new at the top', 'Add new after link ID %s'];
+$GLOBALS['TL_LANG']['tl_links']['toggle'] = ['Publish/unpublish link', 'Publish/unpublish link ID %s'];
