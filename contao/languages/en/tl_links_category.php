925500daa2c919c1c8b8e1dfe8fb2bf2c17f8b5b
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-05 23:27:48 +0330

    format by ecs

--- contao/languages/en/tl_links_category.php
+++ contao/languages/en/tl_links_category.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -8,31 +10,25 @@
  * @license LGPL-3.0-or-later
  */
 
-
-/**
+/*
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_links_category']['title']              = array('Title','Please enter the categhory title.');
-$GLOBALS['TL_LANG']['tl_links_category']['protected']          = array('Protect category','Show link items to certain member groups only.');
-$GLOBALS['TL_LANG']['tl_links_category']['groups']             = array('Allowed member groups','These groups will be able to see the link items in this category.');
+$GLOBALS['TL_LANG']['tl_links_category']['title'] = ['Title', 'Please enter the categhory title.'];
+$GLOBALS['TL_LANG']['tl_links_category']['protected'] = ['Protect category', 'Show link items to certain member groups only.'];
+$GLOBALS['TL_LANG']['tl_links_category']['groups'] = ['Allowed member groups', 'These groups will be able to see the link items in this category.'];
 
-
-/**
+/*
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_links_category']['title_legend']       = 'Title';
-$GLOBALS['TL_LANG']['tl_links_category']['protected_legend']   = 'Access protection';
+$GLOBALS['TL_LANG']['tl_links_category']['title_legend'] = 'Title';
+$GLOBALS['TL_LANG']['tl_links_category']['protected_legend'] = 'Access protection';
 
-
-/**
+/*
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_links_category']['new']                = array('New Category','Add a new categhory');
-$GLOBALS['TL_LANG']['tl_links_category']['edit']               = array('Edit categhory','Edit categhory ID %s');
-$GLOBALS['TL_LANG']['tl_links_category']['editheader']         = array('Edit categhory settings','Edit categhory settings ID %s');
-$GLOBALS['TL_LANG']['tl_links_category']['copy']               = array('Copy categhory','Copy categhory ID %s');
-$GLOBALS['TL_LANG']['tl_links_category']['delete']             = array('Delete categhory','Delete categhory ID %s');
-$GLOBALS['TL_LANG']['tl_links_category']['show']               = array('Category details','Show the details of categhory ID %s');
-
-
-
+$GLOBALS['TL_LANG']['tl_links_category']['new'] = ['New Category', 'Add a new categhory'];
+$GLOBALS['TL_LANG']['tl_links_category']['edit'] = ['Edit categhory', 'Edit categhory ID %s'];
+$GLOBALS['TL_LANG']['tl_links_category']['editheader'] = ['Edit categhory settings', 'Edit categhory settings ID %s'];
+$GLOBALS['TL_LANG']['tl_links_category']['copy'] = ['Copy categhory', 'Copy categhory ID %s'];
+$GLOBALS['TL_LANG']['tl_links_category']['delete'] = ['Delete categhory', 'Delete categhory ID %s'];
+$GLOBALS['TL_LANG']['tl_links_category']['show'] = ['Category details', 'Show the details of categhory ID %s'];
