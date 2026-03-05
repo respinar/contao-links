fe17040ffe433c38687251eae25d7ddec90ff4c6
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-05 23:28:15 +0330

    format by ecs

--- contao/languages/fa/modules.php
+++ contao/languages/fa/modules.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -7,11 +10,9 @@
  * @license LGPL-3.0-or-later
  */
 
-
-$GLOBALS['TL_LANG']['FMD']['links']['0']                = 'پیوندها';
-$GLOBALS['TL_LANG']['FMD']['links']['1']                = 'افزودن فهرست پیوندها به وب‌سایت';
-$GLOBALS['TL_LANG']['MOD']['links']['0']                = 'پیوندها';
-$GLOBALS['TL_LANG']['MOD']['links']['1']                = 'افزودن و مدیریت پیوندها';
-$GLOBALS['TL_LANG']['tl_module']['category_legend']     = 'دسته‌بندی پیوندها';
+$GLOBALS['TL_LANG']['FMD']['links']['0'] = 'پیوندها';
+$GLOBALS['TL_LANG']['FMD']['links']['1'] = 'افزودن فهرست پیوندها به وب‌سایت';
+$GLOBALS['TL_LANG']['MOD']['links']['0'] = 'پیوندها';
+$GLOBALS['TL_LANG']['MOD']['links']['1'] = 'افزودن و مدیریت پیوندها';
+$GLOBALS['TL_LANG']['tl_module']['category_legend'] = 'دسته‌بندی پیوندها';
 $GLOBALS['TL_LANG']['tl_module']['links_category']['0'] = 'دسته‌بندی پیوندها';
-
