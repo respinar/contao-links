3803216028897916ddf468ff2c3db70358d00c76
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-05 23:27:24 +0330

    format by ecs

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -8,9 +10,7 @@
  * @license LGPL-3.0-or-later
  */
 
-
-/**
+/*
  * Miscellaneous
  */
 $GLOBALS['TL_LANG']['MSC']['emptyLinks'] = 'There is no links';
-
