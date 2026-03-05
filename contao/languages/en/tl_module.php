356e7aebf8bfcc52e3dbcf0ad8375280943ec4c6
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-05 23:27:59 +0330

    format by ecs

--- contao/languages/en/tl_module.php
+++ contao/languages/en/tl_module.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -8,17 +10,14 @@
  * @license LGPL-3.0-or-later
  */
 
-
-/**
+/*
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_module']['links_categories']  = array('Links categories','Please select one or more links categories.');
-$GLOBALS['TL_LANG']['tl_module']['links_sortBy']      = array('Links sort by','Here you can select the links sorting option.');
-$GLOBALS['TL_LANG']['tl_module']['links_template']    = array('Links template','Here you can select the links template.');
+$GLOBALS['TL_LANG']['tl_module']['links_categories'] = ['Links categories', 'Please select one or more links categories.'];
+$GLOBALS['TL_LANG']['tl_module']['links_sortBy'] = ['Links sort by', 'Here you can select the links sorting option.'];
+$GLOBALS['TL_LANG']['tl_module']['links_template'] = ['Links template', 'Here you can select the links template.'];
 
-/**
+/*
  * Legends
  */
 $GLOBALS['TL_LANG']['tl_module']['category_legend'] = 'Links categories';
-
-
