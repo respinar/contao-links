635c5e831e947c0cb71c3483467c1286714a62b3
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-06 23:28:12 +0330

    format by ecs

--- contao/languages/fa/tl_links_category.php
+++ contao/languages/fa/tl_links_category.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -7,29 +10,28 @@
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_links_category']['alias']['0']         = 'اسم مستعار';
-$GLOBALS['TL_LANG']['tl_links_category']['alias']['1']         = 'اسم مستعار دسته‌بندی را وارد نمایید.';
-$GLOBALS['TL_LANG']['tl_links_category']['copy']['0']          = 'رونوشت دسته‌بندی';
-$GLOBALS['TL_LANG']['tl_links_category']['copy']['1']          = 'رونوشت دسته‌بندی شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links_category']['delete']['0']        = 'حذف دسته‌بندی';
-$GLOBALS['TL_LANG']['tl_links_category']['delete']['1']        = 'حذف دسته‌بندی شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links_category']['description']['0']   = 'توضیح';
-$GLOBALS['TL_LANG']['tl_links_category']['description']['1']   = 'اینجا می‌توانید توضیحات این دسته‌بندی را وارد نمایید.';
+$GLOBALS['TL_LANG']['tl_links_category']['alias']['0'] = 'اسم مستعار';
+$GLOBALS['TL_LANG']['tl_links_category']['alias']['1'] = 'اسم مستعار دسته‌بندی را وارد نمایید.';
+$GLOBALS['TL_LANG']['tl_links_category']['copy']['0'] = 'رونوشت دسته‌بندی';
+$GLOBALS['TL_LANG']['tl_links_category']['copy']['1'] = 'رونوشت دسته‌بندی شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links_category']['delete']['0'] = 'حذف دسته‌بندی';
+$GLOBALS['TL_LANG']['tl_links_category']['delete']['1'] = 'حذف دسته‌بندی شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links_category']['description']['0'] = 'توضیح';
+$GLOBALS['TL_LANG']['tl_links_category']['description']['1'] = 'اینجا می‌توانید توضیحات این دسته‌بندی را وارد نمایید.';
 $GLOBALS['TL_LANG']['tl_links_category']['description_legend'] = 'توضیح';
-$GLOBALS['TL_LANG']['tl_links_category']['edit']['0']          = 'ویرایش دسته‌بندی';
-$GLOBALS['TL_LANG']['tl_links_category']['edit']['1']          = 'ویرایش دسته‌بندی شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links_category']['editheader']['0']    = 'ویرایش تنظیمات دسته‌بندی';
-$GLOBALS['TL_LANG']['tl_links_category']['editheader']['1']    = 'ویرایش تنظیمات دسته‌بندی شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links_category']['groups']['0']        = 'گروه‌های اعضاء مجاز';
-$GLOBALS['TL_LANG']['tl_links_category']['groups']['1']        = 'این گروه‌ها قادر به دیدن آیتم‌های پیوند در این دسته‌بندی خواهند بود.';
-$GLOBALS['TL_LANG']['tl_links_category']['new']['0']           = 'دسته‌بندی جدید';
-$GLOBALS['TL_LANG']['tl_links_category']['new']['1']           = 'افزودن یک دسته‌بندی جدید';
-$GLOBALS['TL_LANG']['tl_links_category']['protected']['0']     = 'حفاظت از دسته‌بندی';
-$GLOBALS['TL_LANG']['tl_links_category']['protected']['1']     = 'آیتم‌های پیوند را فقط برای گروه خاصی نمایش بده.';
-$GLOBALS['TL_LANG']['tl_links_category']['protected_legend']   = 'حفاظت دسترسی';
-$GLOBALS['TL_LANG']['tl_links_category']['show']['0']          = 'جزئیات دسته‌بندی';
-$GLOBALS['TL_LANG']['tl_links_category']['show']['1']          = 'نمایش جزئیات دسته‌بندی شناسه %s';
-$GLOBALS['TL_LANG']['tl_links_category']['title']['0']         = 'عنوان';
-$GLOBALS['TL_LANG']['tl_links_category']['title']['1']         = 'نام دسته‌بندی را وارد کنید.';
-$GLOBALS['TL_LANG']['tl_links_category']['title_legend']       = 'عنوان';
-
+$GLOBALS['TL_LANG']['tl_links_category']['edit']['0'] = 'ویرایش دسته‌بندی';
+$GLOBALS['TL_LANG']['tl_links_category']['edit']['1'] = 'ویرایش دسته‌بندی شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links_category']['editheader']['0'] = 'ویرایش تنظیمات دسته‌بندی';
+$GLOBALS['TL_LANG']['tl_links_category']['editheader']['1'] = 'ویرایش تنظیمات دسته‌بندی شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links_category']['groups']['0'] = 'گروه‌های اعضاء مجاز';
+$GLOBALS['TL_LANG']['tl_links_category']['groups']['1'] = 'این گروه‌ها قادر به دیدن آیتم‌های پیوند در این دسته‌بندی خواهند بود.';
+$GLOBALS['TL_LANG']['tl_links_category']['new']['0'] = 'دسته‌بندی جدید';
+$GLOBALS['TL_LANG']['tl_links_category']['new']['1'] = 'افزودن یک دسته‌بندی جدید';
+$GLOBALS['TL_LANG']['tl_links_category']['protected']['0'] = 'حفاظت از دسته‌بندی';
+$GLOBALS['TL_LANG']['tl_links_category']['protected']['1'] = 'آیتم‌های پیوند را فقط برای گروه خاصی نمایش بده.';
+$GLOBALS['TL_LANG']['tl_links_category']['protected_legend'] = 'حفاظت دسترسی';
+$GLOBALS['TL_LANG']['tl_links_category']['show']['0'] = 'جزئیات دسته‌بندی';
+$GLOBALS['TL_LANG']['tl_links_category']['show']['1'] = 'نمایش جزئیات دسته‌بندی شناسه %s';
+$GLOBALS['TL_LANG']['tl_links_category']['title']['0'] = 'عنوان';
+$GLOBALS['TL_LANG']['tl_links_category']['title']['1'] = 'نام دسته‌بندی را وارد کنید.';
+$GLOBALS['TL_LANG']['tl_links_category']['title_legend'] = 'عنوان';
