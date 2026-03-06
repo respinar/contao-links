e6e737f0f2ec5a95f6e12a1ed22786b86e97dfc8
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-06 23:28:41 +0330

    format by ecs

--- contao/models/LinksModel.php
+++ contao/models/LinksModel.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -8,98 +10,91 @@
  * @license LGPL-3.0-or-later
  */
 
-
 /**
- * Run in a custom namespace, so the class can be replaced
+ * Run in a custom namespace, so the class can be replaced.
  */
+
 namespace Respinar\Links\Model;
 
 use Contao\Model;
 use Contao\Model\Collection;
 
 /**
- * Reads and writes Members Act
-*/
+ * Reads and writes Members Act.
+ */
 class LinksModel extends Model
 {
+    /**
+     * Table name.
+     *
+     * @var string
+     */
+    protected static $strTable = 'tl_links';
 
-	/**
-	 * Table name
-	 * @var string
-	 */
-	protected static $strTable = 'tl_links';
-	
-	/**
-	 * Find published link items by their parent ID
-	 */
-	public static function findPublishedByPids(array $arrPids, bool $blnFeatured=null, int $intLimit=0, int $intOffset=0, array $arrOptions=array()): Collection|null
-	{
-		if (!is_array($arrPids) || empty($arrPids))
-		{
-			return null;
-		}
+    /**
+     * Find published link items by their parent ID.
+     */
+    public static function findPublishedByPids(array $arrPids, bool|null $blnFeatured = null, int $intLimit = 0, int $intOffset = 0, array $arrOptions = []): Collection|null
+    {
+        if (!\is_array($arrPids) || empty($arrPids)) {
+            return null;
+        }
 
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid IN(".implode(',', array_map('intval', $arrPids)).')'];
 
-		/*if ($blnFeatured === true)
-		{
-			$arrColumns[] = "$t.featured=1";
-		}
-		elseif ($blnFeatured === false)
-		{
-			$arrColumns[] = "$t.featured=''";
-		}*/
+        /*if ($blnFeatured === true)
+        {
+            $arrColumns[] = "$t.featured=1";
+        }
+        elseif ($blnFeatured === false)
+        {
+            $arrColumns[] = "$t.featured=''";
+        }*/
 
-		// Never return unpublished elements in the back end, so they don't end up in the RSS feed
-		if (!static::isPreviewMode($arrOptions))
-		{
-			$time = time();
-			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
-		}
+        // Never return unpublished elements in the back end, so they don't end up in the
+        // RSS feed
+        if (!static::isPreviewMode($arrOptions)) {
+            $time = time();
+            $arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+        }
 
-		if (!isset($arrOptions['order']))
-		{
-			$arrOptions['order']  = "$t.sorting";
-		}
+        if (!isset($arrOptions['order'])) {
+            $arrOptions['order'] = "$t.sorting";
+        }
 
-		$arrOptions['limit']  = $intLimit;
-		$arrOptions['offset'] = $intOffset;
+        $arrOptions['limit'] = $intLimit;
+        $arrOptions['offset'] = $intOffset;
 
-		return static::findBy($arrColumns, null, $arrOptions);
-	}
+        return static::findBy($arrColumns, null, $arrOptions);
+    }
 
+    /**
+     * Count published link items by their parent ID.
+     */
+    public static function countPublishedByPids(array $arrPids, bool|null $blnFeatured = null, array $arrOptions = []): int
+    {
+        if (!\is_array($arrPids) || empty($arrPids)) {
+            return 0;
+        }
 
-	/**
-	 * Count published link items by their parent ID
-	 */
-	public static function countPublishedByPids(array $arrPids, bool $blnFeatured=null, array $arrOptions=array()): int
-	{
-		if (!is_array($arrPids) || empty($arrPids))
-		{
-			return 0;
-		}
+        $t = static::$strTable;
+        $arrColumns = ["$t.pid IN(".implode(',', array_map('intval', $arrPids)).')'];
 
-		$t = static::$strTable;
-		$arrColumns = array("$t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+        /*if ($blnFeatured === true)
+        {
+            $arrColumns[] = "$t.featured=1";
+        }
+        elseif ($blnFeatured === false)
+        {
+            $arrColumns[] = "$t.featured=''";
+        }*/
 
-		/*if ($blnFeatured === true)
-		{
-			$arrColumns[] = "$t.featured=1";
-		}
-		elseif ($blnFeatured === false)
-		{
-			$arrColumns[] = "$t.featured=''";
-		}*/
+        if (!BE_USER_LOGGED_IN) {
+            $time = time();
+            $arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+        }
 
-		if (!BE_USER_LOGGED_IN)
-		{
-			$time = time();
-			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
-		}
-
-		return static::countBy($arrColumns, null, $arrOptions);
-	}
-
-
+        return static::countBy($arrColumns, null, $arrOptions);
+    }
 }
