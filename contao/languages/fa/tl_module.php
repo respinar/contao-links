e2fcce42ba2b1a5636119e1153011fce4a065bcf
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-06 23:28:19 +0330

    format by ecs

--- contao/languages/fa/tl_module.php
+++ contao/languages/fa/tl_module.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -7,12 +10,10 @@
  * @license LGPL-3.0-or-later
  */
 
-
-$GLOBALS['TL_LANG']['tl_module']['category_legend']       = 'دسته‌بندی‌های پیوندها';
+$GLOBALS['TL_LANG']['tl_module']['category_legend'] = 'دسته‌بندی‌های پیوندها';
 $GLOBALS['TL_LANG']['tl_module']['links_categories']['0'] = 'دسته‌بندی‌های پیوندها';
 $GLOBALS['TL_LANG']['tl_module']['links_categories']['1'] = 'لطفا یک یا چند دسته‌بندی پیوندها را انتخاب کنید.';
-$GLOBALS['TL_LANG']['tl_module']['links_sortBy']['0']     = 'مرتب‌سازی پیوند‌ها';
-$GLOBALS['TL_LANG']['tl_module']['links_sortBy']['1']     = 'اینجا می‌توانید گزینه‌ی مرتب‌سازی پیوندها را انتخاب کنید.';
-$GLOBALS['TL_LANG']['tl_module']['links_template']['0']   = 'قالب پیوندها';
-$GLOBALS['TL_LANG']['tl_module']['links_template']['1']   = 'اینجا می‌توانید قالب پیوندها را انتخاب کنید.';
-
+$GLOBALS['TL_LANG']['tl_module']['links_sortBy']['0'] = 'مرتب‌سازی پیوند‌ها';
+$GLOBALS['TL_LANG']['tl_module']['links_sortBy']['1'] = 'اینجا می‌توانید گزینه‌ی مرتب‌سازی پیوندها را انتخاب کنید.';
+$GLOBALS['TL_LANG']['tl_module']['links_template']['0'] = 'قالب پیوندها';
+$GLOBALS['TL_LANG']['tl_module']['links_template']['1'] = 'اینجا می‌توانید قالب پیوندها را انتخاب کنید.';
