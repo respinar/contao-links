27894b1a99d1d6f673d8b754dd774367d1a3af45
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-06 23:28:16 +0330

    format by ecs

--- contao/languages/fa/tl_links.php
+++ contao/languages/fa/tl_links.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -7,48 +10,47 @@
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_links']['addImage']['0']   = 'افزودن تصویر';
-$GLOBALS['TL_LANG']['tl_links']['addImage']['1']   = 'افزودن تصویر به پیوند.';
-$GLOBALS['TL_LANG']['tl_links']['class']['0']      = 'CSS class';
-$GLOBALS['TL_LANG']['tl_links']['class']['1']      = 'خصیصه‌ی class، به خصیصه‌ی class در نشانه‌گذاری HTML اضافه خواهد شد.';
-$GLOBALS['TL_LANG']['tl_links']['copy']['0']       = 'رونوشت پیوند';
-$GLOBALS['TL_LANG']['tl_links']['copy']['1']       = 'رونوشت پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['cut']['0']        = 'جابجایی پیوند';
-$GLOBALS['TL_LANG']['tl_links']['cut']['1']        = 'جابجایی پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['delete']['0']     = 'حذف پیوند';
-$GLOBALS['TL_LANG']['tl_links']['delete']['1']     = 'حذف پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['edit']['0']       = 'ویرایش پیوند';
-$GLOBALS['TL_LANG']['tl_links']['edit']['1']       = 'ویرایش پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['href_legend']     = 'خصیصه‌های ابرپیوند';
-$GLOBALS['TL_LANG']['tl_links']['image_legend']    = 'تنظیمات تصویر';
-$GLOBALS['TL_LANG']['tl_links']['linkTitle']['0']  = 'عنوان پیوند';
-$GLOBALS['TL_LANG']['tl_links']['linkTitle']['1']  = 'عنوان پیوند به خصیصه title در نشانه‌گذاری اچ‌تی‌ام‌ال اضافه می‌شود.';
-$GLOBALS['TL_LANG']['tl_links']['new']['0']        = 'پیوند جدید';
-$GLOBALS['TL_LANG']['tl_links']['new']['1']        = 'افزودن پیوند جدید';
+$GLOBALS['TL_LANG']['tl_links']['addImage']['0'] = 'افزودن تصویر';
+$GLOBALS['TL_LANG']['tl_links']['addImage']['1'] = 'افزودن تصویر به پیوند.';
+$GLOBALS['TL_LANG']['tl_links']['class']['0'] = 'CSS class';
+$GLOBALS['TL_LANG']['tl_links']['class']['1'] = 'خصیصه‌ی class، به خصیصه‌ی class در نشانه‌گذاری HTML اضافه خواهد شد.';
+$GLOBALS['TL_LANG']['tl_links']['copy']['0'] = 'رونوشت پیوند';
+$GLOBALS['TL_LANG']['tl_links']['copy']['1'] = 'رونوشت پیوند شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links']['cut']['0'] = 'جابجایی پیوند';
+$GLOBALS['TL_LANG']['tl_links']['cut']['1'] = 'جابجایی پیوند شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links']['delete']['0'] = 'حذف پیوند';
+$GLOBALS['TL_LANG']['tl_links']['delete']['1'] = 'حذف پیوند شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links']['edit']['0'] = 'ویرایش پیوند';
+$GLOBALS['TL_LANG']['tl_links']['edit']['1'] = 'ویرایش پیوند شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links']['href_legend'] = 'خصیصه‌های ابرپیوند';
+$GLOBALS['TL_LANG']['tl_links']['image_legend'] = 'تنظیمات تصویر';
+$GLOBALS['TL_LANG']['tl_links']['linkTitle']['0'] = 'عنوان پیوند';
+$GLOBALS['TL_LANG']['tl_links']['linkTitle']['1'] = 'عنوان پیوند به خصیصه title در نشانه‌گذاری اچ‌تی‌ام‌ال اضافه می‌شود.';
+$GLOBALS['TL_LANG']['tl_links']['new']['0'] = 'پیوند جدید';
+$GLOBALS['TL_LANG']['tl_links']['new']['1'] = 'افزودن پیوند جدید';
 $GLOBALS['TL_LANG']['tl_links']['pasteafter']['0'] = 'جاگذاری در این دسته';
 $GLOBALS['TL_LANG']['tl_links']['pasteafter']['1'] = 'جاگذاری بعد از پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['pastenew']['0']   = 'افزودن پیوند جدید در بالا';
-$GLOBALS['TL_LANG']['tl_links']['pastenew']['1']   = 'افزودن بعد از پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['publish_legend']  = 'تنظیمات انتشار';
-$GLOBALS['TL_LANG']['tl_links']['published']['0']  = 'انتشار پیوند';
-$GLOBALS['TL_LANG']['tl_links']['published']['1']  = 'پیوند را در وب‌سایت به صورت عمومی نمایش می‌دهد.';
-$GLOBALS['TL_LANG']['tl_links']['rel']['0']        = 'rel';
-$GLOBALS['TL_LANG']['tl_links']['rel']['1']        = 'خصوصیت rel به عنوان خصوصیت rel در نشانه‌‌گذاری HTML اضافه می‌شود (مثلا <em>nofollow</em>).';
-$GLOBALS['TL_LANG']['tl_links']['show']['0']       = 'جزئیات پیوند';
-$GLOBALS['TL_LANG']['tl_links']['show']['1']       = 'نمایش جزئیات پیوند شناسه %s';
-$GLOBALS['TL_LANG']['tl_links']['singleSRC']['0']  = 'پرونده منبع';
-$GLOBALS['TL_LANG']['tl_links']['singleSRC']['1']  = 'لطفا یک پرونده را از پوشه پرونده‌ها انتخاب نمایید.';
-$GLOBALS['TL_LANG']['tl_links']['start']['0']      = 'نمایش از';
-$GLOBALS['TL_LANG']['tl_links']['start']['1']      = 'مورد پیوند را قبل از  این روز در وب‌سایت نمایش نخواهد داد.';
-$GLOBALS['TL_LANG']['tl_links']['stop']['0']       = 'نمایش تا';
-$GLOBALS['TL_LANG']['tl_links']['stop']['1']       = 'مورد پیوند را بعد از  این روز در وب‌سایت نمایش نخواهد داد.';
-$GLOBALS['TL_LANG']['tl_links']['target']['0']     = 'مقصد';
-$GLOBALS['TL_LANG']['tl_links']['target']['1']     = 'باز کردن پیوند در پنجره جدید مرورگر.';
-$GLOBALS['TL_LANG']['tl_links']['title']['0']      = 'عنوان';
-$GLOBALS['TL_LANG']['tl_links']['title']['1']      = 'عنوان پیوند را وارد نمایید.';
-$GLOBALS['TL_LANG']['tl_links']['title_legend']    = 'عنوان';
-$GLOBALS['TL_LANG']['tl_links']['toggle']['0']     = 'انتشار/عدم انتشار پیوند';
-$GLOBALS['TL_LANG']['tl_links']['toggle']['1']     = 'انتشار/عدم انتشار پیوند شناسه‌ی %s';
-$GLOBALS['TL_LANG']['tl_links']['url']['0']        = 'نشانی وب';
-$GLOBALS['TL_LANG']['tl_links']['url']['1']        = 'نشانی وب پیوند را وارد نمایید.';
-
+$GLOBALS['TL_LANG']['tl_links']['pastenew']['0'] = 'افزودن پیوند جدید در بالا';
+$GLOBALS['TL_LANG']['tl_links']['pastenew']['1'] = 'افزودن بعد از پیوند شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links']['publish_legend'] = 'تنظیمات انتشار';
+$GLOBALS['TL_LANG']['tl_links']['published']['0'] = 'انتشار پیوند';
+$GLOBALS['TL_LANG']['tl_links']['published']['1'] = 'پیوند را در وب‌سایت به صورت عمومی نمایش می‌دهد.';
+$GLOBALS['TL_LANG']['tl_links']['rel']['0'] = 'rel';
+$GLOBALS['TL_LANG']['tl_links']['rel']['1'] = 'خصوصیت rel به عنوان خصوصیت rel در نشانه‌‌گذاری HTML اضافه می‌شود (مثلا <em>nofollow</em>).';
+$GLOBALS['TL_LANG']['tl_links']['show']['0'] = 'جزئیات پیوند';
+$GLOBALS['TL_LANG']['tl_links']['show']['1'] = 'نمایش جزئیات پیوند شناسه %s';
+$GLOBALS['TL_LANG']['tl_links']['singleSRC']['0'] = 'پرونده منبع';
+$GLOBALS['TL_LANG']['tl_links']['singleSRC']['1'] = 'لطفا یک پرونده را از پوشه پرونده‌ها انتخاب نمایید.';
+$GLOBALS['TL_LANG']['tl_links']['start']['0'] = 'نمایش از';
+$GLOBALS['TL_LANG']['tl_links']['start']['1'] = 'مورد پیوند را قبل از  این روز در وب‌سایت نمایش نخواهد داد.';
+$GLOBALS['TL_LANG']['tl_links']['stop']['0'] = 'نمایش تا';
+$GLOBALS['TL_LANG']['tl_links']['stop']['1'] = 'مورد پیوند را بعد از  این روز در وب‌سایت نمایش نخواهد داد.';
+$GLOBALS['TL_LANG']['tl_links']['target']['0'] = 'مقصد';
+$GLOBALS['TL_LANG']['tl_links']['target']['1'] = 'باز کردن پیوند در پنجره جدید مرورگر.';
+$GLOBALS['TL_LANG']['tl_links']['title']['0'] = 'عنوان';
+$GLOBALS['TL_LANG']['tl_links']['title']['1'] = 'عنوان پیوند را وارد نمایید.';
+$GLOBALS['TL_LANG']['tl_links']['title_legend'] = 'عنوان';
+$GLOBALS['TL_LANG']['tl_links']['toggle']['0'] = 'انتشار/عدم انتشار پیوند';
+$GLOBALS['TL_LANG']['tl_links']['toggle']['1'] = 'انتشار/عدم انتشار پیوند شناسه‌ی %s';
+$GLOBALS['TL_LANG']['tl_links']['url']['0'] = 'نشانی وب';
+$GLOBALS['TL_LANG']['tl_links']['url']['1'] = 'نشانی وب پیوند را وارد نمایید.';
