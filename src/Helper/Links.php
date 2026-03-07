e6500b7077a3c489ccb292b484722ae00067881f
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-07 23:28:48 +0330

    format by ecs

--- src/Helper/Links.php
+++ src/Helper/Links.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Contao Links Bundle.
  *
@@ -9,127 +11,116 @@
  */
 
 /**
- * Namespace
+ * Namespace.
  */
+
 namespace Respinar\LinksBundle\Helper;
 
 use Contao\CoreBundle\Security\ContaoCorePermissions;
-use Contao\FilesModel;
-use Contao\Frontend;
 use Contao\FrontendTemplate;
 use Contao\Model\Collection;
 use Contao\ModuleModel;
 use Contao\StringUtil;
 use Contao\System;
-use Contao\Validator;
 use Respinar\Links\Model\LinksCategoryModel;
 use Respinar\Links\Model\LinksModel;
 
 /**
- * Class Helper Links
+ * Class Helper Links.
  */
 class Links
 {
-	/**
-	 * URL cache array
-	 * @var artray
-	 */
-	private static $arrUrlCache = array();
+    /**
+     * URL cache array.
+     *
+     * @var artray
+     */
+    private static $arrUrlCache = [];
 
-	/**
-	 * Parse Link
-	 */
-	static public function parseLink(LinksModel $objLink, ModuleModel $model, int $intCount=0): string
-	{
+    /**
+     * Parse Link.
+     */
+    public static function parseLink(LinksModel $objLink, ModuleModel $model, int $intCount = 0): string
+    {
+        $objTemplate = new FrontendTemplate($model->links_template);
 
-		$objTemplate = new FrontendTemplate($model->links_template);
+        $objTemplate->setData($objLink->row());
 
-		$objTemplate->setData($objLink->row());
+        $objTemplate->addImage = false;
 
-		$objTemplate->addImage = false;
+        // Add an image
+        if ($objLink->singleSRC) {
+            if ($model->imgSize) {
+                $size = StringUtil::deserialize($model->imgSize);
 
-		// Add an image
-		if ($objLink->singleSRC)
-		{
-			if ($model->imgSize)
-			{
-				$size = StringUtil::deserialize($model->imgSize);
+                if ($size[0] > 0 || $size[1] > 0 || is_numeric($size[2]) || ($size[2][0] ?? null) === '_') {
+                    $imgSize = $model->imgSize;
+                }
+            }
 
-				if ($size[0] > 0 || $size[1] > 0 || is_numeric($size[2]) || ($size[2][0] ?? null) === '_')
-				{
-					$imgSize = $model->imgSize;
-				}
-			}
+            $figure = System::getContainer()
+                ->get('contao.image.studio')
+                ->createFigureBuilder()
+                ->setSize($imgSize)
+                ->from($objLink->singleSRC)
+                ->buildIfResourceExists()
+            ;
 
-			$figure = System::getContainer()
-				->get('contao.image.studio')
-				->createFigureBuilder()
-				->setSize($imgSize)
-				->from($objLink->singleSRC)
-                ->buildIfResourceExists();
+            $figure->applyLegacyTemplateData($objTemplate);
+        }
 
-			$figure->applyLegacyTemplateData($objTemplate);
-		}
-		
-		$objTemplate->hrefclass = $objLink->class;
-		$objTemplate->linkTitle = $objLink->linkTitle ? $objLink->linkTitle : $objLink->title;
+        $objTemplate->hrefclass = $objLink->class;
+        $objTemplate->linkTitle = $objLink->linkTitle ?: $objLink->title;
 
-		return $objTemplate->parse();
-	}
+        return $objTemplate->parse();
+    }
 
-	/**
-	 * Pars Links
-	 * 
-	 * @return array<string>
-	 * 
-	 */
-	static public function parseLinks(Collection $objLinks, ModuleModel $model): array
-	{
-		$limit = $objLinks->count();
+    /**
+     * Pars Links.
+     *
+     * @return array<string>
+     */
+    public static function parseLinks(Collection $objLinks, ModuleModel $model): array
+    {
+        $limit = $objLinks->count();
 
-		if ($limit < 1)
-		{
-			return array();
-		}
+        if ($limit < 1) {
+            return [];
+        }
 
-		$count = 0;
-		$arrLinks = array();
+        $arrLinks = [];
 
-		foreach ($objLinks as $objLink) {
-			$arrLinks[] = self::parseLink($objLink, $model);
-		}
+        foreach ($objLinks as $objLink) {
+            $arrLinks[] = self::parseLink($objLink, $model);
+        }
 
-		return $arrLinks;
-	}
+        return $arrLinks;
+    }
 
-	/**
-	 * Sort out protected categories
-	 */
-	public static function sortOutProtected(array $arrCategories): array
-	{
-		if (empty($arrCategories) || !\is_array($arrCategories))
-		{
-			return $arrCategories;
-		}
+    /**
+     * Sort out protected categories.
+     */
+    public static function sortOutProtected(array $arrCategories): array
+    {
+        if (empty($arrCategories) || !\is_array($arrCategories)) {
+            return $arrCategories;
+        }
 
-		$objCategory = LinksCategoryModel::findMultipleByIds($arrCategories);
-		$arrCategories = array();
+        $objCategory = LinksCategoryModel::findMultipleByIds($arrCategories);
+        $arrCategories = [];
 
-		if ($objCategory !== null)
-		{
-			$security = System::getContainer()->get('security.helper');
+        if (null !== $objCategory) {
+            $security = System::getContainer()->get('security.helper');
 
-			while ($objCategory->next())
-			{
-				if ($objCategory->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, $objCategory->groups))
-				{
-					continue;
-				}
+            while ($objCategory->next()) {
+                if ($objCategory->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, $objCategory->groups)) {
+                    continue;
+                }
 
-				$arrCategories[] = $objCategory->id;
-			}
-		}
+                $arrCategories[] = $objCategory->id;
+            }
+        }
 
-		return $arrCategories;
-	}
+        return $arrCategories;
+    }
 }
